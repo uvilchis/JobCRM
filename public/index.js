315d968df350be25e4991cbfa2fba1ff50505405
Christine <christinehma214@gmail.com>
2017-11-06 16:43:27 -0500

    added webpack, not working

---
+++ public/index.js
@@ -0,0 +1,5 @@
+//render to react-dom
+import reactDOM from 'react-dom'
+import App from './component/app.js' //var App = require('./component/app.js')
+
+reactDom.render(<App />, document.getElementById('app'))
