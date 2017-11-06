1cd9052c0811ab6a7fc7cce03b44309878904199
Christine <christinehma214@gmail.com>
2017-11-06 16:43:27 -0500

    added webpack, not working

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+module.exports = {
+  entry: './public/index.js',
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ]
+  }
+}
