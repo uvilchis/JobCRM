f4018b15c27deb613b1ad316d7b98cc2fe9e83bc
tommy.york <tommy.york@gmail.com>
2017-11-06 20:24:06 -0500

    cleaned up file structure.

--- public/components/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-var App = function() {
-  return <div> hello </div>
-}
-
-export default App 
--- public/index.js
+++
@@ -1,5 +0,0 @@
-//render to react-dom
-import reactDOM from 'react-dom'
-import App from './component/app.js' //var App = require('./component/app.js')
-
-reactDom.render(<App />, document.getElementById('app'))
