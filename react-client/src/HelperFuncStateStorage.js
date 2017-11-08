652d0e3520f462eb4bd562273d0fc8c00cea4f60
Christine <christinehma214@gmail.com>
2017-11-08 16:58:20 -0500

    input page helper functions working

--- react-client/src/HelperFuncStateStorage.js
+++ react-client/src/HelperFuncStateStorage.js
@@ -4,20 +4,20 @@ export default class HelperFuncStateStorage {
   constructor(props){
   }
 
+  //don't console.log here if you're trying to debug.
+  //console.log where the function is being called before return and after render
+
   updateFieldValue(inst, stateName, e) {
     let stateUpdate = {};
     stateUpdate[stateName] = e.target.value;
     inst.setState(stateUpdate);
-    console.log(inst.state)
     return null
   }
 
   toggleCheckBox(inst, boxName, e) {
     let stateUpdate = {};
-
     stateUpdate[boxName] = !inst.state[boxName]
     inst.setState(stateUpdate)
-    console.log('firstInt>>>>',inst.state)
     return null;
   }
 }
